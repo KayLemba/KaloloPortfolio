07310d2d1a260d93e9b61e218b0a35dd233b9b31
KayLemba <kaylemba@yahoo.com>
2021-11-09 10:38:53 +0200

    Fix eslint error

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-const Bar = document.querySelector('.bars');
-const sideBar = document.querySelector('.sidebar');
-const cancel = document.querySelector('.cancel');
-const anchorLink = document.querySelectorAll('.anchor-link')
-const Body = document.querySelector('body');
+const Bar = document.querySelector(".bars");
+const sideBar = document.querySelector(".sidebar");
+const cancel = document.querySelector(".cancel");
+const anchorLink = document.querySelectorAll(".anchor-link");
+const Body = document.querySelector("body");
 
-Bar.addEventListener('click', openSideBar);
-cancel.addEventListener('click', closeSideBar);
+Bar.addEventListener("click", openSideBar);
+cancel.addEventListener("click", closeSideBar);
 
 anchorLink.forEach((anchor) => {
-    anchor.addEventListener('click', closeSideBar)
-})
+  anchor.addEventListener("click", closeSideBar);
+});
 
 function openSideBar() {
-    sideBar.classList.add('show-side-bar')
-    Body.classList.add('noscroll')
+  sideBar.classList.add("show-side-bar");
+  Body.classList.add("noscroll");
 }
 
 function closeSideBar() {
-    sideBar.classList.remove('show-side-bar')
-    Body.classList.remove('noscroll')
-
+  sideBar.classList.remove("show-side-bar");
+  Body.classList.remove("noscroll");
 }
